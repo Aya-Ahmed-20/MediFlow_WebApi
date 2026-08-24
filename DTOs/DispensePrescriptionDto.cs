e969c1c1f952fb5c68faafe18d2efb163e1e6ca8
Aya-Ahmed-20 <aya992157@gmail.com>
2026-08-25 00:58:12 +0300

    feat(dto): add DispensePrescriptionDto and validation rules
    - Create DispensePrescriptionDto for incoming dispensing requests.
    - Implement FluentValidation rules to ensure data integrity.

---
+++ DTOs/DispensePrescriptionDto.cs
@@ -0,0 +1,12 @@
+﻿using MediFlowApi.Models;
+
+namespace MediFlowApi.DTOs
+{
+    public class DispensePrescriptionDto
+    {
+        public int PrescriptionId { get; set; }
+
+        public string? Notes { get; set; }
+
+    }
+}
---
+++ DTOs/DispensePrescriptionDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace MediFlowApi.DTOs
+{
+    public class DispensePrescriptionDtoValidator :AbstractValidator<DispensePrescriptionDto>
+    {
+          public DispensePrescriptionDtoValidator() 
+        { 
+            RuleFor(x=>x.PrescriptionId).GreaterThan(0);
+            RuleFor(x=>x.Notes).MaximumLength(500);
+        }
+    }
+}
